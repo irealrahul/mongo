ef9a82606d3e4ec626b277ccb94a630c0e204496
irealrahul <rahul@rkgroup.ltd>
2020-09-24 16:24:49 +0530

    new

---
+++ pages/movies.js
@@ -0,0 +1,39 @@
+import { connectToDatabase } from "../util/mongodb";
+
+export default function Movies({ movies }) {
+  return (
+    <div>
+      <h1>address data</h1>
+      <p>
+        <small>(According to Metacritic)</small>
+      </p>
+      <ul>
+        {movies.map((movie) => (
+          <li>
+            <h2>{movie.name}</h2>
+            <h3>{movie.coin}</h3>
+            <p>{movie.balance}</p>
+			<p>{movie.address}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
+
+  const movies = await db
+    .collection("test")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  return {
+    props: {
+      movies: JSON.parse(JSON.stringify(movies)),
+    },
+  };
+}
